be3faac4dacf6b234c25ff36ede0cff8f77f6ce7
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-05-29 19:13:28 -0700

    Point of no return. Compile at end instead

--- controllers/customer-choices.js
+++ controllers/customer-choices.js
@@ -4,11 +4,32 @@ const inquire = require('inquirer');
 
 const customerChoices = () => {
   return inquire
-    .prompt({
-      name: 'userChoice',
-      type: 'input',
-      message: 'What is the ID of the item you would like to purchase? [Quit with Q]',
-    })
+    .prompt([
+      {
+        name: 'item_ID',
+        type: 'input',
+        message: 'What is the ID of the item you would like to purchase? [Quit with Q]',
+        validate: val => {
+          if (isNaN(val) === false) {
+            return true;
+          }
+          console.log('   Provide a number');
+          return false;
+        }
+      },
+      {
+        name: 'qty',
+        type: 'input',
+        message: 'How many would you like?',
+        validate: val => {
+          if (isNaN(val) === false) {
+            return true;
+          }
+          console.log('   Provide a number');
+          return false;
+        }
+      }
+    ])
 }
 
 module.exports = customerChoices;
--- index.js
+++ index.js
@@ -21,53 +21,25 @@ const connection = new MySQL({
 
 //Core logic for app flow. Checks the user's authority and calls the respective functions
 async function start(user) {
-  if(user === 'CUSTOMER') {
 
-  // Show the inventory to the user
-  try {
-    const inventory = await connection.query('SELECT * FROM products');
-      customerView(inventory);
-  } catch (err) {
-    console.error(`Failed to grab store inventory:${err}`);
+  if (user === 'CUSTOMER') {
+
+    // Show the inventory to the user READ
+    await showInventory();
+    // Make a purchase
+    await makePurchase();
+    start(user);
   }
 
-  // Grab the user's shopping choice
-    let item_ID = await customerChoices();
-    item_ID = parseInt(item_ID.userChoice);
-
-    try {
-    // Modify inventory amount UPDATE
-      const rows = await connection.query('SELECT * FROM products WHERE ?',{ item_ID });
-      console.log(rows);
-    } catch (err) {
-      console.error(`Failed to grab item:${err}`);
-    }
-  }
-
-  if(user === 'MANAGER') {
+  if (user === 'MANAGER') {
     managerChoices();
   }
 
-  if(user === 'SUPERVISOR') {
+  if (user === 'SUPERVISOR') {
     supervisorChoices();
   }
 }
 
-// const customerChoices = () => {
-//   console.log('Getting Customer Choices..')
-//   // Request something fromt the DB and pass it to view
-//   connection.query('SELECT * FROM products',(err,res) => console.log(res));
-//   customerView();
-// }
-
-// const customerChoices = () => {
-//   return inquire
-//     .prompt({
-//       name: 'userChoice',
-//       type: 'input',
-//       message: 'What is the ID of the item you would like to purchase? [Quit with Q]'
-//     })
-// }
 
 /**
  * @returns a promise object containing the authority of user as a string
@@ -78,13 +50,57 @@ function getUserType() {
       name: 'status',
       type: 'list',
       message: 'Plese indicate your level of authority',
-      choices: ['CUSTOMER','MANAGER','SUPERVISOR']
+      choices: ['CUSTOMER', 'MANAGER', 'SUPERVISOR']
     })
 }
 
-function displayInventory() {
-  connection.query('SELECT * FROM products', (err) => {
-    if (err) throw err;
+// Read
+async function showInventory() {
+  try {
+    const inventory = await connection.query('SELECT * FROM products');
+    customerView(inventory);
+  } catch (err) {
+    console.error(`Failed to grab store inventory:${err}`);
+  }
+}
 
-  })
+// Update
+async function makePurchase() {
+
+  // Grab the user's shopping choices
+  const { item_ID, qty } = await grabChoices(customerChoices);
+
+  try {
+    // Modify inventory amount -UPDATE
+    const rows = await connection.query('SELECT * FROM products WHERE ?', { item_ID });
+    //If there's enough in stock
+    if (qty <= rows[0].stock_quantity) {
+      connection.query('UPDATE products SET ? WHERE ?', [
+        {
+          stock_quantity: rows[0].stock_quantity - qty
+        },
+        { item_ID }
+      ])
+      console.log(`Successfully purchased ${qty} ${rows[0].product_name}`);
+
+    } else {
+      console.log('Cannot complete this purchase. There are not enough in stock');
+    }
+
+  } catch (err) {
+    console.error(`Failed to grab item:${err}`);
+  }
+}
+/**
+ * @param  {Function} cb is a "choices" callback: customer, manager, or supervisor
+ * @returns and object with the resulting user choices
+ */
+async function grabChoices(cb) {
+  const userChoices = await cb();
+  const item_ID = parseInt(userChoices.item_ID);
+  const qty = parseInt(userChoices.qty);
+  return {
+    item_ID,
+    qty
+  }
 }
